bcc2138d139e31773d3c60e76c1c53aa1271f013
aakashDeveloper <ahanda205@gmail.com>
2020-04-15 17:23:35 +0100

    classComponent

--- firstapp/src/component/Header.js
+++ firstapp/src/component/Header.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React,{Component,Fragment} from 'react';
 
+class Header extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            title:'React App',
+            keyword:'User Text'
+        }
+        
+    }
+
+    inputChanges = (event) => {
+        console.log(event.target.value)
+        this.setState({keyword:event.target.value?event.target.value:'User Text'})                                                                                     
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <center>
+                    <h1>{this.state.title}</h1>
+                    <input onChange={this.inputChanges}/>
+                    <p>{this.state.keyword}</p>
+                </center>
+                <hr/>
+            </Fragment>
+        )
+    }
+}
+
+
+/*
 const Header = () => {
     return(
-        <React.Fragment>
+        <Fragment>
             <center>
                 <h1>React App</h1>
             </center>
             <hr/>
-        </React.Fragment>
+        </Fragment>
     )
 }
+*/
+
 
 export default Header;
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './component/Header';
+import Footer from './component/Footer';
 
 const App = () => {
     return (
         <React.Fragment>
             <Header/>
             <h2>This is React Page</h2>
+            <Footer year="2020"/>
         </React.Fragment>
     )
 }
